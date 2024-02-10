b108ca05f48a080083aff18023f9a1a36277a2f0
ruslkovalen123 <ruslkovalen@gmail.com>
2024-02-10 13:50:05 +0000

    Change imports for getRemoteImageURLs helper

--- src/utils/getRemoteImageURLs.ts
+++ src/utils/getRemoteImageURLs.ts
@@ -1,46 +1,44 @@
 import path from "path";
-import fs from "fs";
-const path2 = require("path");
-const fs2 = require("fs");
-const { promisify } = require('util')
-const directoryPath = path2.join(__dirname, '../../../../../temp_img');
-const readdir = promisify(require('fs').readdir)
-const stat = promisify(require('fs').stat)
+import fs, { readdir as readDir } from "fs";
+import { promisify } from 'util';
+
+const directoryPath = path.join(__dirname, '../../../../temp_img');
+const readdir = promisify(readDir)
 
 const urlToFilename = require("./urlToFilename");
 
-async function GetImages(){
-	let  newArr: string[]= [];
-	let files = await readdir(directoryPath);
-	files.forEach(function (file: any) {
+async function GetImages() {
+  let newArr: string[] = [];
+  let files = await readdir(directoryPath);
+  files.forEach(function (file: any) {
 
-		const contents =  fs2.readFileSync(path2.join(directoryPath, file), 'utf-8');
-		newArr = newArr.concat(JSON.parse(contents));
-    
-	})
-	return newArr;
-	
+    const contents = fs.readFileSync(path.join(directoryPath, file), 'utf-8');
+    newArr = newArr.concat(JSON.parse(contents));
+
+  })
+  return newArr;
 }
+
 export async function getRemoteImageURLs(
   nextConfigFolder: string,
   folderPathForRemoteImages: string
 ) {
 
-  let remoteImageURLs = [];
-  if(!process.env.EXPORT_MODE) {
-  const remoteImagesFilePath = path.join(
-    nextConfigFolder,
-    "remoteOptimizedImages.js"
-  );
-  if (fs.existsSync(remoteImagesFilePath)) {
-    const remoteOptimizedImages = await require(remoteImagesFilePath);
+  let remoteImageURLs: string[] = [];
+  if (!process.env.EXPORT_MODE) {
+    const remoteImagesFilePath = path.join(
+      nextConfigFolder,
+      "remoteOptimizedImages.js"
+    );
+    if (fs.existsSync(remoteImagesFilePath)) {
+      const remoteOptimizedImages = await require(remoteImagesFilePath);
 
-    remoteImageURLs = remoteOptimizedImages;
+      remoteImageURLs = remoteOptimizedImages;
+    }
   }
-}
-else {
-  remoteImageURLs = await GetImages();
-}
+  else {
+    remoteImageURLs = await GetImages();
+  }
   // Create the filenames for the remote images
   const remoteImageFilenames = remoteImageURLs.map((url: string) => {
     const encodedURL = urlToFilename(url);
